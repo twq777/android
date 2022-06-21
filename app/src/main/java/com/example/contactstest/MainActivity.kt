34b15a613a5bfbe40e9d5e7a9e04b0462930970e
twq777 <2875376691@qq.com>
2022-06-21 11:48:15 +0800

    1

---
+++ app/src/main/java/com/example/contactstest/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.contactstest
+
+import android.content.AbstractThreadedSyncAdapter
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.ContactsContract
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+//import java.util.jar.Manifest
+import android.Manifest
+
+class MainActivity : AppCompatActivity() {
+
+    private val contractList = ArrayList<String>()
+    private lateinit var adapter: ArrayAdapter<String>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,contractList)
+        contactsView.adapter = adapter
+        if (ContextCompat.checkSelfPermission(this,Manifest.permission.READ_CONTACTS)!=PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_CONTACTS),1)
+        }else{
+            readContacts()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            1->{
+                if (grantResults.isNotEmpty()&&grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    readContacts()
+                }else{
+                    Toast.makeText(this,"You denied the permission",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+    private fun readContacts(){
+        contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_FILTER_URI,null,null,null,null)?.apply {
+            while (moveToNext()){
+                val displayName = getString(getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME))
+                val number = getString(getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
+                contractList.add("$displayName\n$number")
+            }
+            adapter.notifyDataSetChanged()
+            close()
+        }
+    }
+}
